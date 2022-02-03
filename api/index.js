567f44e1c9295264cf4546fa5de9e86e08af782a
jaidan22 <jaidansleeba22@gmail.com>
2022-02-03 14:15:30 +0530

    updated routes

--- api/index.js
+++ api/index.js
@@ -3,6 +3,8 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const authRoute = require("./routes/auth");
 const usersRoute = require("./routes/users");
+const moviesRoute = require("./routes/movies");
+const listRoute = require("./routes/lists");
 
 dotenv.config();
 
@@ -17,6 +19,8 @@ app.use(express.json());
 
 app.use("/api/auth", authRoute);
 app.use("/api/users", usersRoute);
+app.use("/api/movies", moviesRoute);
+app.use("/api/lists", listRoute);
 
 app.listen(8800, () => {
   console.log("Backend server running in port 8800");
