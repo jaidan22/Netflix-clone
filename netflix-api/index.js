31a968cb7fe593e90a36fead776a87176ca49bdc
jaidan22 <jaidansleeba22@gmail.com>
2023-04-02 10:42:39 +0530

    updated cors

--- netflix-api/index.js
+++ netflix-api/index.js
@@ -9,9 +9,27 @@ const listRoute = require("./routes/lists");
 dotenv.config();
 
 const app = express();
-const cors = require("cors");
 
-app.use(cors());
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "https://clone-netflix-22.netlify.app"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET,OPTIONS,PATCH,DELETE,POST,PUT"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
+  );
+  if (req.method === "OPTIONS") {
+    res.status(200).end();
+    return;
+  }
+  next();
+});
 
 mongoose
   .connect(process.env.DB_URL)
